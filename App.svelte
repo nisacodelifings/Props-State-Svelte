9f315951bc121109e0965d3c6c6bc762fee943ae
Annisa Puspitasari <nisa.codelifings@gmail.com>
2020-09-10 22:28:47 -0700

    mantap

---
+++ App.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Homepage from "./Homepage.svelte";	
+</script>
+
+<Homepage content="ini adalah list" isi="namaku nisa" />
+<Homepage content="ini adalah array" isi="namaku rara" />
+<Homepage content="ini adalah home" isi="namaku juju" />
---
+++ Homepage.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let content;
+	export let isi;
+</script>
+
+<p>{content}</p>
+<ul>
+	<li>{content}</li>
+	<li>{isi}</li>
+	<li>{isi}</li>
+</ul>
